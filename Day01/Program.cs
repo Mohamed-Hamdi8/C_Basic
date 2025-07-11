c0d2d4fdae9c61325f47d4b7f54a01f7a28d7bd1
mohamed hamdi <mohamdi111200444@gmail.com>
2025-07-11 04:09:43 +0300

    Q.1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1- Write a program that allows the user to enter a number then print it.
+            //bool Parsed;
+            //do
+            //{
+            //    Console.Write("Enter Number Plz : ");
+            //    Parsed = int.TryParse(Console.ReadLine(), out int Num);
+            //} while (!Parsed);
+            #endregion
         }
     }
 }
